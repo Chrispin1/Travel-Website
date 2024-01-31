45d155b032e04dab039328ca64da5996d3dff543
Chrispin <kiokondolo1@gmail.com>
2024-01-31 14:25:01 +0300

    created TopBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import TopBar from "./components/TopBar";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Chrispin</h1>
+      <TopBar />
     </>
   );
 }
